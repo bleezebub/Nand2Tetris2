2527c17ecfe35b760c7c80c1b21137fe3b700781
bleezebub <rishabhjain9024@gmail.com>
2022-01-18 13:07:23 +0530

    completed add command

--- 07/Test.asm
+++ 07/Test.asm
@@ -1,9 +1,29 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm
-
-// Pushes and adds two constants.
-push constant 7
-push constant 8
-add
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@0
+M=M-1
+@0
+A=M
+D=M
+@0
+M=M-1
+@0
+A=M
+D=D+M
+@0
+A=M
+M=D
+@0
+M=M+1
--- 07/VMTranslator.cpp
+++ 07/VMTranslator.cpp
@@ -1,5 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
+//asserts that file extensions are correct
 bool checkextensionvm(string fn){
   if(fn.substr(fn.find_last_of(".") + 1) == "vm") {
     return true;
@@ -14,6 +15,8 @@ bool checkextensionasm(string fn){
     return false;
   }
 }
+
+//gets the file name for output file
 string getoutstring(string input){
   int n = input.size();
   for(int i=0;i<n;i++){
@@ -22,6 +25,16 @@ string getoutstring(string input){
     }
   }
   return "error";
+}
+//parses the lines into seperate words
+vector<string> getparsewords(string line){
+  stringstream s(line);
+  string word;
+  vector<string> commands;
+  while(s>>word){
+    commands.push_back(word);
+  }
+  return commands;
 }
 void Translator();
 int main(int argc, char *argv[]){
@@ -33,12 +46,62 @@ int main(int argc, char *argv[]){
   freopen(output.c_str(), "w", stdout);
   Translator();
 }
-void parse(string line){
-  
+/*Implements push constant*/
+void push_constant(std::vector<string> commands){
+    string val = commands.back();
+    cout<<"@"<<val<<endl;
+    cout<<"D=A"<<endl;
+    cout<<"@SP"<<endl;
+    cout<<"A=M"<<endl;
+    cout<<"M=D"<<endl;
+    cout<<"@SP"<<endl;
+    cout<<"M=M+1"<<endl;
+}
+/*Implements add*/
+void add_command(vector<string> commands){
+  cout<<"@0"<<endl;
+  cout<<"M=M-1"<<endl;
+  cout<<"@0"<<endl;
+  cout<<"A=M"<<endl;
+  cout<<"D=M"<<endl;
+  cout<<"@0"<<endl;
+  cout<<"M=M-1"<<endl;
+  cout<<"@0"<<endl;
+  cout<<"A=M"<<endl;
+  cout<<"D=D+M"<<endl;
+  cout<<"@0"<<endl;
+  cout<<"A=M"<<endl;
+  cout<<"M=D"<<endl;
+  cout<<"@0"<<endl;
+  cout<<"M=M+1"<<endl;
+}
+
+void push_command(vector<string> commands){
+  assert(commands[0] == "push");
+
+  if(commands[1] == "constant"){
+    push_constant(commands);
+    return;
+  }
+
+}
+void translate(string line){
+  vector<string> commands = getparsewords(line);
+  assert(commands.size()>0);
+
+  if(commands[0] == "push"){
+    push_command(commands);
+    return;
+  }
+
+  if(commands[0] == "add"){
+    add_command(commands);
+    return;
+  }
 }
 void Translator(){
   std::string line;
   while (std::getline(std::cin, line)){
-    parse(line);
+    translate(line);
   }
 }
